c281317e3bd603684b9ddfa21aebf353fd0b8e87
johnk2280 <johnk2280@gmail.com>
2022-07-27 22:15:37 +0500

    [add] create VPS and get VPS list

--- vps_manager/mainapp/views.py
+++ vps_manager/mainapp/views.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
+from django_filters.rest_framework import DjangoFilterBackend
+
 from .models import VPS
 from .serializers import VPSSerializer
 
@@ -7,3 +9,5 @@ from .serializers import VPSSerializer
 class VPSModelViewSet(ModelViewSet):
     queryset = VPS.objects.all()
     serializer_class = VPSSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('uid', 'cpu', 'ram', 'hdd', 'status')
