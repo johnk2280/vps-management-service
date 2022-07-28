9ee6841923e812cb81d509ee1a96403042b82526
johnk2280 <johnk2280@gmail.com>
2022-07-28 22:20:08 +0500

    [add] vps_manager service to docker-compose.yml

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.10.4
+
+RUN apt-get update &&  \
+    apt-get install -y postgresql postgresql-contrib libpq-dev python3-dev
+
+RUN pip3 install --upgrade pip
+
+COPY ./backend/vps_manager ./
+COPY ./requirements.txt ./
+
+RUN pip3 install -r requirements.txt
--- backend/vps_manager/vps_manager/settings.py
+++ backend/vps_manager/vps_manager/settings.py
@@ -85,10 +85,11 @@ WSGI_APPLICATION = 'vps_manager.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': 'localhost',
+        'NAME': 'vps_test',
+        'USER': 'johnk',
+        'PASSWORD': 'johnk123',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
